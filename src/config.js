62a6c9ee18efd3d12a288297c5eb465cad578bb8
Sunny Lee <leesunny507@gmail.com>
2020-04-27 03:05:32 -0400

    changed code to connect to heroku database

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    APIEndpoint: 'http://localhost:8000/api', 
+    //APIEndpoint: 'http://localhost:8000/api', 
+    APIEndpoint: 'https://guarded-dusk-14196.herokuapp.com/api',
     API_KEY: process.env.REACT_APP_API_KEY
 }
